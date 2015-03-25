26019e21666963456c5fd7132de676c00ee2016e
yuanqing <hello@yuanqing.sg>
2015-03-25 19:27:45 +0800

    Edit test.sh

--- test.sh
+++ test.sh
@@ -2,13 +2,13 @@
 exit_code=0
 test_script="./test.sh"
 for dir in *; do
-  if [ -d "$dir" ]; then
-    cd "$dir"
-    chmod +x "$test_script" && "$test_script"
-    if [ "$?" -ne 0 ]; then
+  if [ -d $dir ]; then
+    cd $dir
+    bash $test_script
+    if [ $? -ne 0 ]; then
       exit_code=1
     fi
     cd ..
   fi
 done
-exit "$exit_code"
+exit $exit_code
